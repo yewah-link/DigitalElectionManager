1b4689c4622f25a4791a036b84d27e2ff1dc7cd0
yewah-link <abrahamyewah@gmail.com>
2024-11-04 08:40:20 +0300

    Initial commit of Digital Election Management project

---
+++ ElectionManagement/src/main/java/com/election/ElectionManagement/ElectionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.election.ElectionManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionManagementApplication.class, args);
+	}
+
+}
---
+++ ElectionManagement/src/test/java/com/election/ElectionManagement/ElectionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.election.ElectionManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
